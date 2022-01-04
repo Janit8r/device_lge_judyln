6a92e40b91c36a2a7d2ee9e556c99143742a1d06
Tuan Anh <60842977+log1cs@users.noreply.github.com>
2022-01-04 18:32:45 +0700

    Update lineage_judyln.mk

--- lineage_judyln.mk
+++ lineage_judyln.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Inherit some AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
+
 # Inherit from judyln device
 $(call inherit-product, device/lge/judyln/device.mk)
 
