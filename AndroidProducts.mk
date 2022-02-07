5cbc432bec6c82dd27b8b80e5165f369302b4015
Tuan Anh <60842977+log1cs@users.noreply.github.com>
2022-02-07 19:19:46 +0700

    Lineage -> AOSP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,9 +1,9 @@
 # Product makefile location
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_judyln.mk
+    $(LOCAL_DIR)/aosp_judyln.mk
 
 # Most common lunch choices
 COMMON_LUNCH_CHOICES := \
-    lineage_judyln-eng \
-    lineage_judyln-userdebug \
-    lineage_judyln-user
+   aosp_judyln-eng \
+   aosp_judyln-userdebug \
+   aosp_judyln-user
