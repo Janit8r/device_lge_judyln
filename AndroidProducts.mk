37ca4fec96e743b3f3dd332d06c56c9108edb3fe
Tuan Anh <60842977+log1cs@users.noreply.github.com>
2022-02-08 20:06:11 +0700

    Radiant bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,9 +1,9 @@
 # Product makefile location
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_judyln.mk
+    $(LOCAL_DIR)/radiant_judyln.mk
 
 # Most common lunch choices
 COMMON_LUNCH_CHOICES := \
-   aosp_judyln-eng \
-   aosp_judyln-userdebug \
-   aosp_judyln-user
+   radiant_judyln-eng \
+   radiant_judyln-userdebug \
+   radiant_judyln-user
