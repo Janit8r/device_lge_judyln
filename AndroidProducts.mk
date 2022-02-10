154dba51a221cc452382c163824ff74cd44a6bfe
Tuan Anh <60842977+log1cs@users.noreply.github.com>
2022-02-10 12:52:49 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,9 +1,9 @@
 # Product makefile location
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/radiant_judyln.mk
+    $(LOCAL_DIR)/awaken_judyln.mk
 
 # Most common lunch choices
 COMMON_LUNCH_CHOICES := \
-   radiant_judyln-eng \
-   radiant_judyln-userdebug \
-   radiant_judyln-user
+   awaken_judyln-eng \
+   awaken_judyln-userdebug \
+   awaken_judyln-user
