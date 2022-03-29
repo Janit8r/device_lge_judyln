1577ddb19177970a8441feb93379d0e017af885c
Tuan Anh <60842977+log1cs@users.noreply.github.com>
2022-03-29 10:33:40 +0700

    Awaken bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,9 +1,9 @@
 # Product makefile location
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_judyln.mk
+    $(LOCAL_DIR)/awaken_judyln.mk
 
 # Most common lunch choices
 COMMON_LUNCH_CHOICES := \
-   aosp_judyln-eng \
-   aosp_judyln-userdebug \
-   aosp_judyln-user
+   awaken_judyln-eng \
+   awaken_judyln-userdebug \
+   awaken_judyln-user
